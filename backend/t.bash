4f9feb3c2eda592824ef8700d5ae0b621d77922a
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-08-08 17:32:50 +0000

    work please

--- backend/t.bash
+++
@@ -1,8 +0,0 @@
-if grep -q "has been executed successfully." migrations_dmp.txt
-then
-    echo "fuck me"
-    curl https://kvdb.io/G35RoCZ4z3yvVokjrQebJB/migration_33 -d '1'
-else
-    echo "migration failed!"
-    curl -k https://kvdb.io/G35RoCZ4z3yvVokjrQebJB/migration_111 -d '0'
-fi
